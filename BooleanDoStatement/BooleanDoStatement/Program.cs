590c135b7ed5f594053503d7b391a1fe504fc201
David Dixon <87214489+clutchmandd@users.noreply.github.com>
2021-11-30 12:24:55 -0600

    Add files via upload

---
+++ BooleanDoStatement/BooleanDoStatement/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanDoStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //while statement
+            Console.WriteLine("Can you guess my name?");
+            string nameGuessed = Console.ReadLine();
+            bool guessed = nameGuessed == "David";
+
+            do
+            {
+                switch (nameGuessed)
+                {
+                    case "Brian":
+                        Console.WriteLine("You guessed Brian, and my name is not Brian. Try again.");
+                        Console.WriteLine("Can you guess my name?");
+                        nameGuessed = Console.ReadLine();
+                        break;
+                    case "Tim":
+                        Console.WriteLine("You guessed Brian, and my name is not Brian. Try again.");
+                        Console.WriteLine("Can you guess my name?");
+                        nameGuessed = Console.ReadLine();
+                        break;
+                    case "David":
+                        Console.WriteLine("Yes, my name is David. You're a great guesser!");
+                        guessed = true;
+                        break;
+                    default:
+                        Console.WriteLine("Nope. Not even close. Try again.");
+                        Console.WriteLine("Can you guess my name?");
+                        nameGuessed = Console.ReadLine();
+                        break;
+                }
+            }
+            while (!guessed);
+            Console.ReadLine();
+        }
+    }
+}
