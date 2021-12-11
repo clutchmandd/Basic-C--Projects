70eefb5d4e160cdd4f6f27558feb404ffdc637a9
David Dixon <87214489+clutchmandd@users.noreply.github.com>
2021-12-10 20:25:29 -0600

    Add files via upload

---
+++ CallingMathMethodsApp/CallingMathMethodsApp/Math.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMathMethodsApp
+{
+    public class MyMath
+    {
+        //Calling input parameter and returning resulting integer
+        public static int Addition(int num1)
+        {
+            return num1 + 5;
+        }
+
+        //Calling input parameter and returning resulting integer
+        public static int Subraction(int num1)
+        {
+            return num1 - 5;
+        }
+
+        //Calling input parameter and returning resulting integer
+        public static int Multiplication(int num1)
+        {
+            return num1 * 5;
+        }
+    }
+}
---
+++ CallingMathMethodsApp/CallingMathMethodsApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMathMethodsApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number you would like to do a few math operations on.");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+
+            //Passing in parameter of userNumber to MyMath Addition method
+            int result1 = MyMath.Addition(userNumber);
+
+            //Passing in parameter of userNumber to MyMath Subtraction method
+            int result2 = MyMath.Subraction(userNumber);
+
+            //Passing in parameter of userNumber to MyMath Addition method
+            int result3 = MyMath.Multiplication(userNumber);
+
+            //Displaying the returned integer results
+            Console.WriteLine("Your number + 5 = " + result1);
+            Console.WriteLine("Your number - 5 = " + result2);
+            Console.WriteLine("Your number x 5 = " + result3);
+            Console.ReadLine();
+        }
+    }
+}
