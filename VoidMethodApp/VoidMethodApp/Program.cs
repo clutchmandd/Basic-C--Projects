f5097100991d01c051626ee3670ad0b767624c0e
David Dixon <87214489+clutchmandd@users.noreply.github.com>
2021-12-13 17:17:49 -0600

    Add files via upload

---
+++ VoidMethodApp/VoidMethodApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Instantiating myMethod
+            myClass myAdditionObject = new myClass();
+            int num1 = 10;
+            int num2 = 5;
+
+            myAdditionObject.myMethod(num1, num2);
+        }
+    }
+}
---
+++ VoidMethodApp/VoidMethodApp/myClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethodApp
+{
+    public class myClass
+    {
+        public void myMethod(int num1, int num2)
+        {
+            int result = num1 + 25;
+            Console.WriteLine(result);
+            Console.WriteLine(num2);
+            Console.ReadLine();
+        }
+    }
+}
