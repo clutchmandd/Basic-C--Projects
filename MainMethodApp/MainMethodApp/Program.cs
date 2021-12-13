6bca1c5ac55ff4d9ee8757aae7b69f4a39966def
David Dixon <87214489+clutchmandd@users.noreply.github.com>
2021-12-13 14:45:11 -0600

    Add files via upload

---
+++ MainMethodApp/MainMethodApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Instantiating myMath method and calling on results
+            myMath mymath = new myMath();
+
+            //Passing in parameters to myMath method
+            int result1 = mymath.myMethod(5);
+            int result2 = mymath.myMethod(4.5);
+            int result3 = mymath.myMethod("10");
+
+            //Displaying resulting integers to the console
+            Console.WriteLine(result1);
+            Console.WriteLine(result2);
+            Console.WriteLine(result3);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MainMethodApp/MainMethodApp/myMath.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodApp
+{
+    public class myMath
+    {
+        //Creating methods in the class myMath with parameters to return an integer to the Main method
+        public int myMethod(int num1)
+        {
+            return num1 + 5;
+        }
+        //Declaring a decimal parameter and converting it to an integer to return an integer
+        public int myMethod(double num2)
+        {
+            int num3 = Convert.ToInt32(num2);
+            return num3 - 1;
+        }
+        //Declaring a string parameter and converting it to an integer to return an integer
+        public int myMethod(string num4)
+        {
+            int num5 = Convert.ToInt32(num4);
+            return num5 * 10;
+        }
+
+    }
+}
