521805a3019dae3cd0ad3467c23c228bc1cc158b
David Dixon <87214489+clutchmandd@users.noreply.github.com>
2021-12-12 18:55:08 -0600

    Add files via upload

---
+++ MainMethodApp/MainMethodApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Instantiating myMath method and calling on results
+            myMath mymath = new myMath();
+
+            int result1 = mymath.Addition();
+
+            int result2 = mymath.Subtraction();
+
+            int result3 = mymath.Multiplication();
+
+            //Displaying resulting integers to the console
+            Console.WriteLine(result1);
+            Console.WriteLine(result2);
+            Console.WriteLine(result3);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MainMethodApp/MainMethodApp/myMath.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodApp
+{
+    public class myMath
+    {
+        //Creating methods in the class myMath with parameters to return an integer to the Main method
+        public int Addition(int num1 = 3)
+        {
+            return num1 + 5;
+        }
+        //Declaring a decimal parameter and converting it to an integer to return an integer
+        public int Subtraction(double num2 = 4.5)
+        {
+            int num3 = Convert.ToInt32(num2);
+            return num3 - 1;
+        }
+        //Declaring a string parameter and converting it to an integer to return an integer
+        public int Multiplication(string num4 = "10")
+        {
+            int num5 = Convert.ToInt32(num4);
+            return num5 * 10;
+        }
+
+    }
+}
