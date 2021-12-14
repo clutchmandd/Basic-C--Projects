6a1a6f067440fd3541bc004c4ca740f78b35d25e
David Dixon <87214489+clutchmandd@users.noreply.github.com>
2021-12-13 20:25:27 -0600

    Add files via upload

---
+++ OverloadMethodApp/OverloadMethodApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadMethodApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number.");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+
+            //Instantiating myClass and passing in the user's input to a void method that outputs an integer
+            myClass myDivisionObject = new myClass();
+            myDivisionObject.divideMethod(userNumber);
+
+            //Displaying the output integer of the overloaded void method
+            myDivisionObject.divideMethod(userNumber, 5);
+
+            //Defining a local variable
+            int a = 100;
+
+            Console.WriteLine("Before getValue method is called, a = {0}", a);
+            Console.ReadLine();
+            
+            //Retreiving the output parameter of the getValue method
+            myDivisionObject.getValue(out a);
+            Console.WriteLine("After the getValue method is called, a = {0}", a);
+            Console.ReadLine();
+
+            //Calling static Class and passing in parameters
+            int result = Add.Sum(10, 25);
+            Console.WriteLine("Using a static class, 10 + 25 = {0}", result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ OverloadMethodApp/OverloadMethodApp/myClass.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadMethodApp
+{
+    public class myClass
+    {
+        //Creating a void method that outputs the user's integer divided by 2
+        public void divideMethod(int num1)
+        {
+            int result = num1 / 2;
+            Console.WriteLine("Your number divided by 2 = " + result);
+            Console.ReadLine();
+        }
+
+        //Overloading the divideMethod
+        public void divideMethod(int num2, int num3)
+        {
+            int result = num2 * num3;
+            Console.WriteLine("Your number multiplied by 5 = " + result);
+            Console.ReadLine();
+        }
+
+        //Creating an overloaded method with output parameters
+        public void getValue(out int x)
+        {
+            int temp = 10;
+            x = temp;
+        }
+        //public int getValue(out int y)
+        //{
+        //    y = 20;
+        //    return y;
+        //}
+    }
+    //Creating a static class
+    public static class Add
+    {
+        public static int Sum(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
