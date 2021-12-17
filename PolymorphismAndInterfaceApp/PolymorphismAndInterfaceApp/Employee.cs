3259a6656e4faf2b24cbca048f7347eaa335d74d
David Dixon <87214489+clutchmandd@users.noreply.github.com>
2021-12-17 15:27:19 -0600

    Add files via upload

---
+++ PolymorphismAndInterfaceApp/PolymorphismAndInterfaceApp/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismAndInterfaceApp
+{
+    //Creating an Employee class that inherets from the abstract class Person as well as the Interface IQuittable
+    public class Employee : Person, IQuittable
+    {
+        //Implementing the virtual method that was inhereted from the abstract class
+        public override void SayName()
+        {
+            base.SayName();
+        }     
+        //Creating an object of IQuittable
+        public void Quit(Employee employee)
+        {
+            Console.WriteLine("Name: " + firstName + " " + lastName + " is quitting.");
+        }
+    }
+}
---
+++ PolymorphismAndInterfaceApp/PolymorphismAndInterfaceApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismAndInterfaceApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiating an employee object and passing in variables
+            Employee employee = new Employee();
+            employee.firstName = "Sample";
+            employee.lastName = "Student";
+
+            //Calling the Interface method Quit()
+            employee.Quit(employee);
+            Console.ReadLine();
+        }
+    }
+}
