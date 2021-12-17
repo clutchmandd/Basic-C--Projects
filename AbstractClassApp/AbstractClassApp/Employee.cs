62bae02a26cc82395448ba7d7e7cbab7f47494e7
David Dixon <87214489+clutchmandd@users.noreply.github.com>
2021-12-17 14:17:33 -0600

    Add files via upload

---
+++ AbstractClassApp/AbstractClassApp/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassApp
+{
+    //Creating an Employee class that inherets from the abstract class Person
+    public class Employee : Person
+    {
+        //Implementing the virtual method that was inhereted from the abstract class
+        public override void SayName()
+        {
+            base.SayName();
+        }
+    }
+}
---
+++ AbstractClassApp/AbstractClassApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiating an employee object and passing in variables
+            Employee employee = new Employee();
+            employee.firstName = "Sample";
+            employee.lastName = "Student";
+
+            //Calling the SayName method that was inhereted from the abstract class
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
