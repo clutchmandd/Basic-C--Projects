5dd835da4d73417fc5d16ba6479d7f44c902f4ec
David Dixon <87214489+clutchmandd@users.noreply.github.com>
2021-12-19 15:55:30 -0600

    Add files via upload

---
+++ EmployeeComparisonApp/EmployeeComparisonApp/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeComparisonApp
+{
+    public class Employee
+    {
+        //Getting the Id properties
+        public int Id { get; set; }
+
+        //Using 2 bool methods to overload the operators and find out if the 2 Employee objects are equal
+        public static bool operator == (Employee employee1, Employee employee2)
+        {
+            return employee1.Id == employee2.Id;
+        }
+        public static bool operator != (Employee employee1, Employee employee2)
+        {
+            return employee1.Id != employee2.Id;
+        }
+    }
+}
---
+++ EmployeeComparisonApp/EmployeeComparisonApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeComparisonApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Instantiating the class Employee with 2 objects
+            Employee employee1 = new Employee();
+            Employee employee2 = new Employee();
+
+            //Declaring employee Id
+            employee1.Id = 5;
+            employee2.Id = 5;
+
+            //Retreiving the correct return of the 2 bool methods
+            Console.WriteLine(employee1 == employee2);
+            Console.ReadLine();
+        }
+    }
+}
