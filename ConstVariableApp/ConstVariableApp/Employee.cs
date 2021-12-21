4bbfe91381e0e66fbae6281f0df6a52050fc3f36
David Dixon <87214489+clutchmandd@users.noreply.github.com>
2021-12-21 15:37:17 -0600

    Add files via upload

---
+++ ConstVariableApp/ConstVariableApp/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstVariableApp
+{
+    public class Employee
+    {
+        //Creating a constructor
+        public Employee()
+        {
+            Console.WriteLine("Constructor");
+        }
+        //Creating a chained constructor
+        public Employee(string fName) : this()
+        {
+            Console.WriteLine("Chained Constructor");
+        }
+        //Creating another chained constructor
+        public Employee(string fName, string lName) : this("David")
+        {
+            Console.WriteLine("Another Chained Constructor");
+        }
+    }
+}
---
+++ ConstVariableApp/ConstVariableApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstVariableApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Instantiating a new object of Employee
+            Employee emp1 = new Employee("Jessica", "Dixon");
+
+            //Creating a constant variable
+            const string name = "David";
+            Console.WriteLine("Hello, my name is {0}, and it is stored as a constant.", name);
+
+            //Creating a var variable
+            var myList = new List<string>() { "apples", "oranges", "bananas" };
+            Console.WriteLine(myList.Contains("apples"));
+            Console.ReadLine();
+        }
+    }
+}
