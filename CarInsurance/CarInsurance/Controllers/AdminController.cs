0fa9db6200243be940c8a32f16314482db5f382f
David Dixon <87214489+clutchmandd@users.noreply.github.com>
2021-12-30 14:17:54 -0600

    CarInsurance

---
+++ CarInsurance/CarInsurance/Controllers/AdminController.cs
@@ -0,0 +1,34 @@
+﻿using CarInsurance.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarInsurance.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            using (InsuranceEntities db = new InsuranceEntities())
+            {
+                var signups = db.Insurees.ToList();
+                var signupVms = new List<Insuree>();
+                foreach (var signup in signups)
+                {
+                    var signupVm = new Insuree();
+                    signupVm.Id = signup.Id;
+                    signupVm.FirstName = signup.FirstName;
+                    signupVm.LastName = signup.LastName;
+                    signupVm.EmailAddress = signup.EmailAddress;
+                    signupVm.Quote = signup.Quote;
+                    signupVms.Add(signupVm);
+                }
+
+                return View(signupVms);
+            }
+        }
+    }
+}
