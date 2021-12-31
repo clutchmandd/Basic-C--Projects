c5cc54df138d12a920cc1429448edd0f4d8d7e15
David Dixon <87214489+clutchmandd@users.noreply.github.com>
2021-12-30 18:27:01 -0600

    CodeFirstApp

---
+++ CodeFirstApp/CodeFirstApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CodeFirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var student = new Student() { StudentName = "David Dixon" };
+
+                ctx.Students.Add(student);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
